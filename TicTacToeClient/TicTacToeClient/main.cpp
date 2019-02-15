6ddbe77e583a43ffaacd8a0b1fec49281d13ace0
huydinh3010 <32333862+huydinh3010@users.noreply.github.com>
2019-02-15 21:09:21 +0700

    first commit

---
+++ TicTacToeClient/TicTacToeClient/main.cpp
@@ -0,0 +1,460 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <string>
+#include<thread>
+#include<Windows.h>
+#define IP "127.0.0.1"
+#define PORT 2222
+#define WINDOW_NAME "TicTacToe"
+
+
+
+using namespace sf;
+using namespace std;
+bool close = false;
+int id = -1;
+string name = "";
+int opponent_id;
+string opponent_name;
+bool turn = true;
+bool changeState = false;
+
+class State {
+protected:
+	Font font;
+	TcpSocket * socket;
+public:
+	virtual void runThread() = 0;
+	virtual void processEvent(Event &event) = 0;
+	virtual void draw(RenderWindow * window) = 0;
+};
+
+class Intro : public State {
+private:
+	bool finding;
+	string strPlayer;
+	Socket::Status status;
+	string strStatus;
+	string statusRec;
+	bool isConnected;
+	bool nameSeted;
+	void connectToServer() {
+		
+		if(!isConnected && socket->connect(IP, PORT) != Socket::Done) strStatus = "Cannot connect to server!";
+		else {
+			if (isConnected) {
+				Packet packet;
+				packet << "players";
+				socket->send(packet);
+			}
+			isConnected = true;
+			strStatus = "Connected! ";
+			do {
+				Packet packet;
+				if (socket->receive(packet) != Socket::Done) {
+					strStatus = "Disconnected!";
+					isConnected = false;
+					nameSeted = false;
+					break;
+				}
+				string str;
+				packet >> str;
+				if (str == "id") {
+					packet >> id;
+					strStatus += "Your id: " + to_string(id);
+				}
+				else if (str == "players") {
+					int players;
+					int matches;
+					packet >> players >> matches;
+					strPlayer = "Players: " + to_string(players) + " | Matches: " + to_string(matches);
+				}
+				else if (str == "your_opponent") {
+					packet >> opponent_id >> opponent_name >> turn;
+					changeState = true;
+					finding = false;
+					statusRec = "Random Match";
+					break;
+				}
+			} while (true);
+		}
+	}
+public:
+	Intro(TcpSocket & socket) {
+		this->font.loadFromFile("Font/BrushScriptStd.otf");
+		this->strStatus = "Connecting to server...";
+		this->isConnected = false;
+		this->nameSeted = false;
+		this->statusRec = "";
+		this->socket = &socket;
+		this->finding = false;
+	}
+	void runThread() {
+		connectToServer();
+	}
+	void draw(RenderWindow * window) {
+		window->clear(Color(249, 204, 118));
+		RectangleShape recOutline(Vector2f(312, 42));
+		recOutline.setPosition(69, 259);
+		recOutline.setFillColor(Color::Red);
+		window->draw(recOutline);
+
+		RectangleShape rec(Vector2f(300, 30));
+		rec.setPosition(75, 265);
+		if (nameSeted) rec.setFillColor(Color::Blue);
+		else rec.setFillColor(Color::White);
+		window->draw(rec);
+		// ten game
+		Text text;
+		text.setFont(font);
+		text.setCharacterSize(40);
+		text.setFillColor(Color::Black);
+		text.setString("TicTacToe");
+		text.setPosition(225 - text.getLocalBounds().width / 2, 10);
+		window->draw(text);
+		int height = text.getLocalBounds().height;
+		// ten tac gia
+		text.setCharacterSize(24);
+		text.setString("Code by Nguyen Huy Dinh");
+		text.setPosition(225 - text.getLocalBounds().width / 2, height + 45);
+		window->draw(text);
+		height += text.getLocalBounds().height;
+		// status
+		text.setString(strStatus);
+		text.setPosition(225 - text.getLocalBounds().width / 2, height + 80);
+		window->draw(text);
+		height += text.getLocalBounds().height;
+		// so nguoi choi, so tran
+		text.setString(strPlayer);
+		text.setPosition(225 - text.getLocalBounds().width / 2, height + 100);
+		window->draw(text);
+		// nickname
+		if (!nameSeted) text.setString("Your Nickname:");
+		else text.setString("Your Nickname: " + name);
+		text.setPosition(225 - text.getLocalBounds().width / 2, height + 150);
+		window->draw(text);
+		// name
+		if (isConnected) {
+			if (!nameSeted) text.setString(name + '|');
+			else text.setString(statusRec);
+			text.setPosition(225 - text.getLocalBounds().width / 2, 263);
+			window->draw(text);
+		}
+		window->display();
+	}
+	void processEvent(Event &event) {
+		if (event.type == Event::KeyPressed) {
+			if (isConnected && !nameSeted) {
+				if (event.key.code >= Keyboard::A && event.key.code <= Keyboard::Z && name.length() <= 15)
+					name += 'a' + event.key.code;
+				else if (event.key.code == Keyboard::BackSpace && name.length() > 0)
+					name.pop_back();
+				else if (event.key.code == Keyboard::Enter && name.length() > 0) {
+					// dang nhap thanh cong
+					Packet packet;
+					packet << "set_name" << name;
+					socket->send(packet);
+					nameSeted = true;
+					statusRec = "Random Match";
+				}
+			}
+		}
+		else if (event.type == Event::MouseButtonPressed) {
+			if (event.mouseButton.button == Mouse::Left && nameSeted && !finding) {
+				int x = event.mouseButton.x;
+				int y = event.mouseButton.y;
+				if (x >= 75 && x <= 375 && y >= 265 && y <= 295) {
+					Packet packet;
+					packet << "find_opponent";
+					socket->send(packet);
+					statusRec = "Finding opponent...";
+					finding = true;
+				}
+			}
+		}
+	}	
+};
+
+class Game : public State {
+private:
+	bool rematchButton;
+	bool backButton;
+	bool opponent;
+	bool showBox;
+	string strBox = "Wait for opponent";
+	int value;
+	bool yourTurn;
+	int matrix[3][3] = {};
+	void drawBackGround(RenderWindow * window) {
+		RectangleShape line1(Vector2f(80, 6));
+		line1.setOrigin(Vector2f(40, 3));
+		line1.setFillColor(Color::Yellow);
+		line1.rotate(90);
+		line1.setPosition(222,40);
+		window->draw(line1);
+		RectangleShape line2(Vector2f(450, 6));
+		line2.setOrigin(Vector2f(0, 3));
+		line2.setFillColor(Color::Yellow);
+		line2.setPosition(0, 80);
+		window->draw(line2);
+		for (int i = 0; i < 4; i++) {
+			RectangleShape line(Vector2f(450, 6));
+			line.setOrigin(Vector2f(0, 3));
+			line.setPosition(0, i * 150 + 100);
+			line.setFillColor(Color(0, 140, 94));
+			window->draw(line);
+		}
+		for (int j = 0; j < 4; j++) {
+			RectangleShape line(Vector2f(450, 6));
+			line.setOrigin(Vector2f(0, 3));
+			line.setPosition(j * 150, 100);
+			line.setFillColor(Color(0, 140, 94));
+			line.rotate(90);
+			window->draw(line);
+		}
+		Text text;
+		// id
+		text.setFont(font);
+		text.setCharacterSize(24);
+		text.setFillColor(Color::Black);
+		text.setPosition(10, 0);
+		text.setString("id: " + to_string(id));
+		window->draw(text);
+		// name
+		text.setString(name);
+		text.setPosition(10, 30);
+		window->draw(text);
+		// opponent id
+		if (opponent) {
+			text.setString("id: " + to_string(opponent_id));
+			text.setPosition(450 - text.getLocalBounds().width - 10, 0);
+			window->draw(text);
+			// opponent name
+			text.setString(opponent_name);
+			text.setPosition(450 - text.getLocalBounds().width - 10, 30);
+			window->draw(text);
+		}
+		// turn
+		text.setString("turn");
+		text.setFillColor(Color::Red);
+		if (yourTurn) text.setPosition(225 - text.getLocalBounds().width - 10, 0);
+		else text.setPosition(230, 0);
+		window->draw(text);
+		// box
+	}
+	void drawMatrix(RenderWindow * window) {
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				if (matrix[i][j] == 1) drawX(window, j, i);
+				else if (matrix[i][j] == -1) drawO(window, j, i);
+	}
+	void drawX(RenderWindow * window, int x, int y) {
+		RectangleShape line1(Vector2f(150, 6));
+		line1.setFillColor(Color::Blue);
+		line1.rotate(45);
+		line1.setOrigin(75, 3);
+		line1.setPosition(x * 150 + 75, y * 150 + 175);
+		RectangleShape line2(Vector2f(150, 6));
+		line2.setFillColor(Color::Blue);
+		line2.rotate(-45);
+		line2.setOrigin(75, 3);
+		line2.setPosition(x * 150 + 75, y * 150 + 175);
+		window->draw(line1);
+		window->draw(line2);
+	}
+	void drawO(RenderWindow * window, int x, int y) {
+		CircleShape circle(52, 100);
+		circle.setFillColor(Color(249, 204, 118));
+		circle.setOrigin(52, 52);
+		circle.setOutlineThickness(6);
+		circle.setOutlineColor(Color::Red);
+		circle.setPosition(x * 150 + 75, y * 150 + 175);
+		window->draw(circle);
+	}
+	void drawBox(RenderWindow * window) {
+		RectangleShape shape(Vector2f(300, 150));
+		shape.setFillColor(Color::White);
+		shape.setOrigin(150, 75);
+		shape.setPosition(225, 275);
+		window->draw(shape);
+		Text textButton;
+		textButton.setFont(font);
+		// strBox
+		textButton.setCharacterSize(24);
+		textButton.setString(strBox);
+		textButton.setFillColor(Color::Black);
+		textButton.setPosition(225 - textButton.getLocalBounds().width / 2, 250);
+		window->draw(textButton);
+		// button
+		textButton.setCharacterSize(30);
+		if (rematchButton) textButton.setFillColor(Color::Blue);
+		else textButton.setFillColor(Color::Cyan);
+		textButton.setStyle(Text::Underlined | Text::Bold);
+		textButton.setString("Rematch");
+		textButton.setPosition(100, 300);
+		window->draw(textButton);
+		//cout << textButton.getPosition().x << " " << textButton.getPosition().y << endl;
+		//cout << textButton.getLocalBounds().width << " " << textButton.getLocalBounds().height << endl;
+		//
+		if (backButton) textButton.setFillColor(Color::Blue);
+		else textButton.setFillColor(Color::Cyan);
+		textButton.setString("Back");
+		textButton.setPosition(290, 300);
+		window->draw(textButton);
+		/*cout << textButton.getPosition().x << " " << textButton.getPosition().y << endl;
+		cout << textButton.getLocalBounds().width << " " << textButton.getLocalBounds().height << endl;*/
+	}
+	void listenFromSocket() {
+		do {
+			Packet packet;
+			if (socket->receive(packet) != Socket::Done) {
+				changeState = true;
+				break;
+			}
+			string str;
+			packet >> str;
+			if (str == "set") {
+				int x, y;
+				packet >> x >> y;
+				matrix[y][x] = -value;
+				yourTurn = true;
+			}
+			else if (str == "exit") { // chấp nhận yc 
+				changeState = true;
+				break;
+			}
+			else if (str == "you_win") {
+				showBox = true;
+				strBox = "You Win";
+				if (opponent) rematchButton = true;
+				else rematchButton = false;
+			}
+			else if (str == "draw") {
+				showBox = true;
+				strBox = "Draw";
+				if (opponent) rematchButton = true;
+				else rematchButton = false;
+			}
+			else if (str == "you_lose") {
+				showBox = true;
+				strBox = "You Lose";
+				if (opponent) rematchButton = true;
+				else rematchButton = false;
+			}
+			else if (str == "rematch") {
+				packet >> yourTurn;
+				for (int i = 0; i < 3; i++)
+					for (int j = 0; j < 3; j++)
+						matrix[i][j] = 0;
+				this->showBox = false;
+				this->opponent = true;
+				this->backButton = true;
+				this->rematchButton = true;
+				if (yourTurn) this->value = 1;
+				else this->value = -1;
+			}
+			else if (str == "opponent_out") {
+				showBox = true;
+				strBox = "Your opponent is out!";
+				rematchButton = false;
+				opponent = false;
+			}
+		} while (true);
+	}
+	
+public:
+	Game(TcpSocket &socket) {
+		this->socket = &socket;
+		this->font.loadFromFile("Font/BrushScriptStd.otf");
+		if (turn) this->value = 1;
+		else this->value = -1;
+		this->yourTurn = turn;
+		this->showBox = false;
+		this->opponent = true;
+		this->backButton = true;
+		this->rematchButton = true;
+	}
+	void processEvent(Event &event) {
+		if (event.type == Event::MouseButtonPressed) {
+			int x = event.mouseButton.x;
+			int y = event.mouseButton.y;
+			if (event.mouseButton.button == Mouse::Left && yourTurn && !showBox) {
+				x = x / 150;
+				y = (y - 100) / 150;
+				if (x >= 0 && x <= 2 && y >= 0 && y <= 2 && matrix[y][x] == 0) {
+					matrix[y][x] = value;
+					Packet packet;
+					packet << "set" << x << y << value;
+					socket->send(packet);
+					yourTurn = false;
+				}
+			}
+			else if (showBox) {
+				// bam vao nut
+				// rematch 100 300 - 110 24;
+				// back 290 300 - 65 23
+				if (rematchButton && x >= 100 && x <= 210 && y >= 300 && y <= 324) {
+					// bam vao nut rematch
+					Packet packet;
+					packet << "rematch";
+					socket->send(packet);
+					strBox = "Wait for opponent...";
+					rematchButton = false;
+				}
+				else if (backButton && x >= 290 && x <= 355 && y >= 300 && y <= 323) {
+					// bam vao nut back
+					// gui toi server
+					Packet packet;
+					packet << "exit_game";
+					socket->send(packet);
+					backButton = false;
+				}
+			}
+		}
+	}
+	void draw(RenderWindow * window) {
+		window->clear(Color(249, 204, 118));
+		drawBackGround(window);
+		drawMatrix(window);
+		if (showBox) drawBox(window);
+		window->display();
+	}
+	void runThread() {
+		listenFromSocket();
+	}
+};
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow){
+	RenderWindow window(VideoMode(450, 550), WINDOW_NAME, Style::Close);
+	TcpSocket socket;
+	State * stateIntro = new Intro(socket);
+	State * state = stateIntro;
+	thread * t_thread = new thread(&State::runThread, state);
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed) {
+				socket.disconnect();
+				window.close();
+			}
+			state->processEvent(event);
+		}
+		state->draw(&window);
+		if (changeState) {
+			if (state == stateIntro) state = new Game(socket);
+			else {
+				delete state;
+				state = stateIntro;
+				
+			}
+			t_thread->join();
+			delete t_thread;
+			t_thread = new thread(&State::runThread, state);
+			changeState = false;
+		}
+		Sleep(20);
+	}
+	t_thread->join();
+	return 0;
+}
---
+++ TicTacToeServer/TicTacToeServer/main.cpp
@@ -0,0 +1,294 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <thread>
+#include <string>
+#include <vector>
+#include <mutex>
+#include <algorithm>
+#include <iterator>
+#include <Windows.h>
+#define PORT 2222
+
+using namespace sf;
+using namespace std;	
+
+bool id[1000] = {};
+mutex m;
+class Game {
+public:
+	/*TcpSocket * socket1;
+	TcpSocket * socket2;*/
+	int matrix[3][3] = {};
+	void set(int x, int y, int value) {
+		matrix[y][x] = value;
+	}
+	void clear() {
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				matrix[i][j] = 0;
+	}
+	int checkWin(int value) {
+		if (matrix[0][0] == value && matrix[0][1] == value && matrix[0][2] == value) return 1;
+		if (matrix[1][0] == value && matrix[1][1] == value && matrix[1][2] == value) return 1;
+		if (matrix[2][0] == value && matrix[2][1] == value && matrix[2][2] == value) return 1;
+		if (matrix[0][0] == value && matrix[1][0] == value && matrix[2][0] == value) return 1;
+		if (matrix[0][1] == value && matrix[1][1] == value && matrix[2][1] == value) return 1;
+		if (matrix[0][2] == value && matrix[1][2] == value && matrix[2][2] == value) return 1;
+		if (matrix[0][0] == value && matrix[1][1] == value && matrix[2][2] == value) return 1;
+		if (matrix[2][0] == value && matrix[1][1] == value && matrix[0][2] == value) return 1;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (matrix[i][j] == 0) return -1; // chưa kết thúc
+			}
+		}
+		return 0; // hòa
+	}
+}; 
+vector<Game*> listGames;
+
+class Player {
+public:
+	Game * game;
+	TcpSocket * socket;
+	Player * opponent;
+	int id;
+	string name;
+	bool inMatch;
+	bool rematch;
+	Player(TcpSocket * socket, int id) {
+		this->socket = socket;
+		this->id = id;
+		this->inMatch = false;
+		this->rematch = false;
+	}
+	~Player() {
+		if (game != NULL) {
+			m.lock();
+			vector<Game *>::iterator it = find(listGames.begin(), listGames.end(), game);
+			if (it != listGames.end()) listGames.erase(it);
+			m.unlock();
+			delete game;
+			cout << "A game is delete" << endl;
+		}
+		delete socket;
+		if (opponent != NULL) {
+			opponent->opponent = NULL;
+			opponent->game = NULL;
+		}
+	}
+};
+
+vector<Player *> listPlayers; // danh sách người kết nối với server
+vector<Player *> players; // danh sách những người đang tìm đối thủ
+
+
+
+void listenFromSocket(Player * player) {
+	do {
+		Packet packet;
+		if (player->socket->receive(packet) != Socket::Done) {
+			if (player->inMatch && player->opponent != NULL) {
+				Packet p;
+				p << "opponent_out";
+				player->opponent->socket->send(p);
+				player->opponent->opponent = NULL;
+				player->opponent->game = NULL;
+			}
+			m.lock();
+			vector<Player*>::iterator it;
+			it = find(listPlayers.begin(), listPlayers.end(), player);
+			if (it != listPlayers.end()) listPlayers.erase(it);
+			it = find(players.begin(), players.end(), player);
+			if (it != players.end()) players.erase(it);
+			Packet packet;
+			packet << "players" << listPlayers.size() << listGames.size();
+			for (int i = 0; i < listPlayers.size(); i++) {
+				if (!listPlayers[i]->inMatch) listPlayers[i]->socket->send(packet);
+			}
+			m.unlock();
+			id[player->id] = false;
+			delete player;
+			//player = NULL;
+			cout << "A player is disconnect" << endl;
+			break;
+		}
+		string str;
+		packet >> str;
+		if (str == "set_name") packet >> player->name;
+		else if (str == "player") {
+			Packet packet;
+			m.lock();
+			int list_size = listPlayers.size();
+			int games = listGames.size();
+			m.unlock();
+			packet << "players" << list_size << games;
+			player->socket->send(packet);
+		}
+		else if (str == "find_opponent") {
+			m.lock();
+			players.push_back(player);
+			m.unlock();
+		}
+		else if (str == "players") {
+			m.lock();
+			Packet p;
+			p << listPlayers.size() << listGames.size();
+			player->socket->send(p);
+			m.unlock();
+		}
+		else if (str == "set") {
+			int x, y, value;
+			packet >> x >> y >> value;
+			player->game->set(x, y, value);
+			Packet pk;
+			pk << "set" << x << y << value;
+			if(player->opponent != NULL) player->opponent->socket->send(pk);
+			int k = player->game->checkWin(value);
+			if (k == 0) {
+				Packet p;
+				p << "draw";
+				player->socket->send(p);
+				if (player->opponent != NULL) player->opponent->socket->send(p);
+			}
+			else if (k == 1) {
+				Packet p1;
+				p1 << "you_win";
+				player->socket->send(p1);
+				Packet p2;
+				p2 << "you_lose";
+				if (player->opponent != NULL) player->opponent->socket->send(p2);
+			}
+		}
+		else if (str == "rematch") {
+			player->rematch = true;
+			if (player->opponent != NULL && player->opponent->rematch) {
+				Packet p;
+				p << "rematch";
+				player->socket->send(p);
+				player->opponent->socket->send(p);
+				player->rematch = false;
+				player->opponent->rematch = false;
+				player->game->clear();
+			}
+		}
+		else if (str == "exit_game") {
+			Packet p1;
+			p1 << "exit";
+			player->socket->send(p1);
+			player->inMatch = false;
+			// del game
+			if (player->game != NULL) {
+				m.lock();
+				vector<Game *>::iterator it = find(listGames.begin(), listGames.end(), player->game);
+				if (it != listGames.end()) listGames.erase(it);
+				m.unlock();
+				delete player->game;
+				player->game = NULL;
+				cout << "A game is delete" << endl;
+				
+			}
+			
+			//player->game = NULL;
+			if (player->opponent != NULL) {
+				Packet p2;
+				p2 << "opponent_out";
+				player->opponent->socket->send(p2);
+				player->opponent->opponent = NULL;
+				player->opponent->game = NULL;
+				player->opponent = NULL;
+			}
+		}
+	} while (true);
+}
+
+void setGame() {
+	do {
+		Sleep(100);
+		m.lock();
+		int k = players.size();
+		m.unlock();
+		if (k > 1) {
+			// trong vector players có thể có con trỏ null?
+			m.lock();
+ 			Player * player1 = players.back();
+			players.pop_back();
+			Player * player2 = players.back();
+			players.pop_back();
+			m.unlock();
+			// tạo game
+			player1->inMatch = true;
+			player2->inMatch = true;
+			Game * game = new Game();
+			m.lock();
+			listGames.push_back(game);
+			m.unlock();
+			player1->game = game;
+			player2->game = game;
+			player1->opponent = player2;
+			player2->opponent = player1;
+			// gửi thông tin đối thủ cho 2 bên
+			bool a = rand() % 2;
+			Packet packet1, packet2;
+			packet1 << "your_opponent" << player2->id << player2->name << a;
+			packet2 << "your_opponent" << player1->id << player1->name << !a;
+			player1->socket->send(packet1);
+			player2->socket->send(packet2);
+			m.lock();
+			Packet packet;
+			packet << "players" << listPlayers.size() << listGames.size();
+			for (int i = 0; i < listPlayers.size(); i++) {
+				if (!listPlayers[i]->inMatch) listPlayers[i]->socket->send(packet);
+			}
+			m.unlock();
+			//
+		}
+	} while (true);
+}
+
+int main(){
+	srand(time(NULL));
+	TcpListener listener;
+	if (listener.listen(PORT) != Socket::Done) {
+		cout << "Loi khi lang nghe ket noi" << endl;
+		system("pause");
+	}
+	thread th(setGame);
+	
+	do {
+		
+		TcpSocket *client = new TcpSocket();
+		if (listener.accept(*client) != Socket::Done) {
+			cout << "Loi khi nhan ket noi tu client" << endl;
+			delete client;
+		}
+		else {
+			int idClient;
+			do {
+				idClient = rand() % 999 + 1;
+			} while (id[idClient]);
+			id[idClient] = true;
+			Player * player = new Player(client, idClient);
+			
+			m.lock();
+			listPlayers.push_back(player);
+			int size_p = listPlayers.size();
+			int size_g = listGames.size();
+			m.unlock();
+			Packet packet;
+			packet << "id" << idClient;
+			client->send(packet);
+			packet.clear();
+			packet << "players" << size_p << size_g;
+			m.lock();
+			for (int i = 0; i < listPlayers.size(); i++) {
+				if (!listPlayers[i]->inMatch) listPlayers[i]->socket->send(packet);
+			}
+			m.unlock();
+			cout << "A player connected to server. Id: " << idClient << endl;
+			thread * fun = new thread(listenFromSocket, player);
+		}
+	} while (true);
+	system("pause");
+	return 0;
+}
